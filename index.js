89e8be33928b51e4fe50363efd97d9b030c320b4
boscocj16 <boscojoseph79@gmail.com>
2025-02-24 21:28:33 +0530

    3.2

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 
 const persons = [
-    { id: "1", name: "Arto Hellas", number: "040-123456" },
-    { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-    { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" }
 ];
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
+app.get('/info', (req, res) => {
+    const numEntries = persons.length;
+    const currentTime = new Date();
+
+    res.send(`
+        <p>Phonebook has info for ${numEntries} people</p>
+        <p>${currentTime}</p>
+    `);
 });
 
 const PORT = 3001;

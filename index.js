40f55a83c3f59f7b22af632422cfd31820fa26f1
boscocj16 <boscojoseph79@gmail.com>
2025-02-24 21:31:18 +0530

    3.3

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const app = express();
 
 const persons = [
     { id: 1, name: "Arto Hellas", number: "040-123456" },
-    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" }
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
 
-app.get('/info', (req, res) => {
-    const numEntries = persons.length;
-    const currentTime = new Date();
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);  
+    const person = persons.find(p => p.id === id);
 
-    res.send(`
-        <p>Phonebook has info for ${numEntries} people</p>
-        <p>${currentTime}</p>
-    `);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).json({ error: "Person not found" });
+    }
 });
 
 const PORT = 3001;

aa98118bf49cda50f33ed5bd73e37ce24a34ab7e
boscocj16 <boscojoseph79@gmail.com>
2025-02-24 21:38:58 +0530

    3.4

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+app.use(express.json());
+
+let persons = [
     { id: 1, name: "Arto Hellas", number: "040-123456" },
     { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
     { id: 3, name: "Dan Abramov", number: "12-43-234345" },
     { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);  
-    const person = persons.find(p => p.id === id);
 
-    if (person) {
-        res.json(person);
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const initialLength = persons.length;
+    
+    persons = persons.filter(person => person.id !== id);
+
+    if (persons.length < initialLength) {
+        res.status(204).end(); 
     } else {
         res.status(404).json({ error: "Person not found" });
     }
